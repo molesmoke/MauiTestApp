b74d3106ad454874854e7886215973095174cd6b
Jeremy Powell <jeremy@visionaid.com>
2023-10-30 16:00:22 +1300

    Add animation

--- MauiTestApp/MainPage.xaml.cs
+++ MauiTestApp/MainPage.xaml.cs
@@ -2,23 +2,29 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+
+        botImage.Opacity = 1;
+
+        Animation animation = new(v => botImage.Opacity = v, botImage.Opacity, 0);
+        animation.StartDelay = 10000;
+        animation.Commit(botImage, nameof(botImage));
+    }
 }
 
